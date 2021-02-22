ff773fec366c32e943d09f67c8c2734682e07e00
NIKHIL BN <45630095+NBNARADHYA@users.noreply.github.com>
2021-02-22 18:33:53 +0530

    override:  image.tag  (#10)
    * override: helm value image.tag with the commit sha
    
    * add: kube ca and config files to gitignore
    
    * Restyled by shellharden
    
    * Restyled by shfmt
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- .github/scripts/helm_deploy.sh
+++ .github/scripts/helm_deploy.sh
@@ -5,10 +5,11 @@ SERVICE_NAME=$1
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" >ca.crt
 
-helm upgrade $SERVICE_NAME ./k8s/helm \
-    --install \
-    --kube-apiserver "$KUBERNETES_SERVER" \
-    --kube-ca-file ca.crt \
-    --kube-token "$KUBERNETES_TOKEN" \
-    --kubeconfig /dev/null \
-    -f ./services/$SERVICE_NAME/helm-values.yaml
+helm upgrade "$SERVICE_NAME" ./k8s/helm \
+  --install \
+  --kube-apiserver "$KUBERNETES_SERVER" \
+  --kube-ca-file ca.crt \
+  --kube-token "$KUBERNETES_TOKEN" \
+  --kubeconfig /dev/null \
+  -f ./services/"$SERVICE_NAME"/helm-values.yaml \
+  --set image.tag="$IMAGE_TAG"
