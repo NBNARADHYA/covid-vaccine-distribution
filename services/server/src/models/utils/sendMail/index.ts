45f0cc1dc47f2776451f054dedbe1a5c9e54b7e1
Nikhil BN <nikhilbnaradhya1.gem@gmail.com>
2021-02-27 18:00:10 +0530

    configure: server to use custom domain email to send mail

--- services/server/src/models/utils/sendMail/index.ts
+++ services/server/src/models/utils/sendMail/index.ts
@@ -8,12 +8,13 @@ export interface EmailProps {
 
 export const sendMail = async ({ to, html, subject }: EmailProps) => {
   const transporter = createTransport({
-    service: "gmail",
+    service: process.env.SMTP_SERVICE,
     host: process.env.SMTP_HOST!,
     auth: {
       user: process.env.SMTP_USER!,
       pass: process.env.SMTP_PASS!,
     },
+    ignoreTLS: Boolean(process.env.SMTP_IGNORE_TLS),
   });
 
   await transporter.sendMail({
